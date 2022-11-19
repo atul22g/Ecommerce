4a9495d450f18638e6f4070829aa71424f0e990c
Atugatran <atugatran@gmail.com>
2022-11-19 17:28:46 +0530

    Add singleProduct

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -48,7 +48,7 @@ const AddToCart = ({ product }) => {
         setIncrease={setIncrease}
       />
 
-      <NavLink to="/cart" onClick={() => addToCart(id, color, amount, product)}>
+      <NavLink to="/Ecommerce/cart" onClick={() => addToCart(id, color, amount, product)}>
         <Button className="btn">Add To Cart</Button>
       </NavLink>
     </Wrapper>
